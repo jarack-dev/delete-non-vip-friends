21b92cba4dadb810630591e4dc0b2abc177b56a3
jared <jared.a.rackley@gmail.com>
2025-02-12 20:42:28 -0800

    adding gui for selecting language, added logger, splitting up files

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ import re
 from PIL import ImageGrab, ImageEnhance
 from pynput import keyboard
 from difflib import SequenceMatcher
+import importlib
+import setup
+from setup import get_user_language, get_line_count, initialize_logger, validate_file
 
-debug = True
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 pyautogui.PAUSE = 1
 script_paused = False
+
 coordinates = {
-    "friendCount": (125, 110, 146.15, 130),
+    "friendCount": {
+        "english": (125, 110, 146.15, 130),
+        "japanese": (85.5, 110, 105, 130),
+        "chinese": ()
+    },
     "friendCode": (175, 70, 270, 95),
     "goToFriend": (150, 200),
     "removeFriend": (150, 410),
@@ -24,49 +31,51 @@ coordinates = {
     "betweenFriendSpace": (135, 235),
 }
 
-# saving this in case i need it later
-# "friendCount": (125, 110, 20, 15),
-# "friendCode": (175, 75, 95, 20),
-# screenshot = pyautogui.screenshot(region=ss_coordinates)
+
+def get_resize_resolution(is_square):
+    selected_language = setup.selected_language
+    if selected_language == "english":
+        return (200, 200) if is_square else (400, 100)
+    elif selected_language == "japanese":
+        return (200, 200) if is_square else (400, 100)
+    elif selected_language == "chinese":
+        return (200, 200) if is_square else (400, 100)
 
 
 def convert_screenshot_to_string(ss_name, ss_coordinates, is_square, psm=7):
-    resize_resolution = (200, 200) if is_square else (400, 100)
+    logger = setup.logger
+    resize_resolution = get_resize_resolution(is_square)
     screenshot = ImageGrab.grab(bbox=ss_coordinates).resize(resize_resolution)
     enhanced_screenshot = ImageEnhance.Contrast(screenshot).enhance(3)
     parsed_text = pytesseract.image_to_string(enhanced_screenshot, lang='eng', config=f'--psm {psm}')
 
-    if debug:
+    if setup.is_debug_mode:
         screenshot.save(ss_name + '.png')
         enhanced_screenshot.save(ss_name + "_enhanced.png")
-        print("parsed text: " + parsed_text)
-        print("level of confidence:", pytesseract.image_to_data(enhanced_screenshot, lang='eng', config=f'--psm {psm}', output_type="dict")["conf"])
+        logger.info(f"parsed text: {parsed_text}")
+        logger.info(f"level of confidence: {pytesseract.image_to_data(enhanced_screenshot, lang='eng', config=f'--psm {psm}', output_type='dict')['conf']}")
     return parsed_text
 
 
 def is_vip_friend(filePath, friend_code):
+    logger = setup.logger
     with open(filePath, 'r') as file:
         for line in file:
             friend_code_from_file = re.sub(r"\D", "", line)
             similarity_score = SequenceMatcher(None, friend_code, friend_code_from_file).ratio()
-            if debug:
-                print(f"{friend_code} and {friend_code_from_file} are {similarity_score * 100}% similar")
+            if setup.is_debug_mode:
+                setup.logger.debug(f"{friend_code} and {friend_code_from_file} are {similarity_score * 100}% similar")
 
             if similarity_score > 0.6:
-                print(f"{friend_code} and {friend_code_from_file} are {similarity_score * 100}% similar, they are a vip")
+                logger.info(f"{friend_code} and {friend_code_from_file} are {similarity_score * 100}% similar, they"
+                            " are a vip")
                 return True
-    print("they are not a vip")
+    setup.logger.info("they are not a vip, the similarity scores were not high enough.")
     return False
 
 
-def get_line_count(filePath):
-    with open(filePath, 'r') as fp:
-        lines = len(fp.readlines())
-        return lines
-
-
 def delete_friend():
-    print("deleting friend...")
+    setup.logger.info("deleting friend...")
     pyautogui.click(coordinates["removeFriend"])
     pyautogui.click(coordinates["confirmDeleteFriend"])
     pyautogui.click(coordinates["clickOutOfProfile"])
@@ -74,18 +83,18 @@ def delete_friend():
 
 def on_press(key):
     global script_paused
+    logger = setup.logger
     try:
-        print(key.char)
         # If key pressed is a special key like shift key.char throws an exception
         if key.char == "q":
-            print("quit hotkey was detected, quitting...")
+            logger.info("quit hotkey was detected, quitting...")
             os.kill(os.getpid(), signal.SIGINT)
         if key.char == "r":
             script_paused = False
-            print("script is resuming...")
+            logger.info("script is resuming...")
         if key.char == "p":
             script_paused = True
-            print("script is pausing...")
+            logger.info("script is pausing...")
     except AttributeError:
         pass
 
@@ -94,20 +103,23 @@ def go_to_next_friend(num_vip_friends):
     if num_vip_friends % 3 == 0:
         pyautogui.moveTo(coordinates["betweenFriendSpace"])
         pyautogui.dragTo(y=coordinates["betweenFriendSpace"][1] - 170, duration=1)
-        coordinates["goToFriend"] = (150, coordinates["goToFriend"][1] - 200) # moving back to first slot
+        coordinates["goToFriend"] = (150, coordinates["goToFriend"][1] - 200)  # moving back to first slot
     else:
         coordinates["goToFriend"] = (150, coordinates["goToFriend"][1] + 100)
 
 
 def check_and_delete_friends():
+    logger = setup.logger
     num_vip_friends = 0
-    current_count_string = (re.sub(r"\D", "", convert_screenshot_to_string("friendCount", coordinates["friendCount"], True)))
+    current_count_string = (
+        re.sub(r"\D", "",
+               convert_screenshot_to_string("friendCount", coordinates["friendCount"][setup.selected_language], True)))
     current_count = int(current_count_string) if current_count_string.isdecimal() else -1
     desired_count = get_line_count("vip_ids.txt")
 
     if current_count == -1:
-        print("the current count could not be found. Make sure you are in the Friends screen and see 'number of "
-              "friends' at the top")
+        logger.error("the current count could not be found. Make sure you are in the Friends screen and see "
+                     "'number of friends' at the top")
         sys.exit(-1)
 
     while current_count > desired_count:
@@ -120,12 +132,14 @@ def check_and_delete_friends():
         index = 0
         while fc == "" or len(fc) < 4:
             if index == 50:
-                print("could not accurately determine the friend code. Exiting...")
+                logger.error("could not accurately determine the friend code. Exiting...")
                 sys.exit(-1)
             index += 1
             psm = 6 if index % 2 == 0 else 7
-            fc = re.sub(r"\D", "", convert_screenshot_to_string("friendCode", coordinates["friendCode"], False, psm))
-        print(fc)
+            fc = re.sub(r"\D", "",
+                        convert_screenshot_to_string("friendCode", coordinates["friendCode"], False, psm))
+        if setup.is_debug_mode:
+            logger.debug(fc)
         if is_vip_friend("vip_ids.txt", fc):
             pyautogui.click(coordinates["clickOutOfProfile"])
             num_vip_friends += 1
@@ -136,6 +150,8 @@ def check_and_delete_friends():
             current_count -= 1
             pyautogui.moveTo(x, y)
         pyautogui.moveTo(x, y)
+    logger.info(f"current count: {current_count}\n desired count: {desired_count}")
+    logger.info("the script has finished running.")
 
 
 if __name__ == '__main__':
@@ -143,6 +159,11 @@ if __name__ == '__main__':
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
     try:
+        get_user_language()
+        validate_file()
+        initialize_logger()
+        importlib.reload(setup)
         check_and_delete_friends()
     except KeyboardInterrupt:
+        setup.logger.error("There was a keyboard interrupt. Exiting...")
         sys.exit()
---
+++ setup.py
@@ -0,0 +1,74 @@
+import logging
+import tkinter as tk
+import sv_ttk
+from tkinter import ttk
+import sys
+
+logger = logging.getLogger("delete_non_vip_friends")
+supported_languages = "english", "japanese", "chinese"
+selected_language = ""
+is_debug_mode = False
+id_file = "vip_ids.txt"
+
+
+def initialize_logger():
+    global logger
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+    error_log = logging.FileHandler("error.log")
+    debug_log = logging.FileHandler("debug.log")
+    error_log.setFormatter(formatter)
+    debug_log.setFormatter(formatter)
+    debug_log.setLevel(logging.DEBUG)
+    error_log.setLevel(logging.ERROR)
+    logger.addHandler(error_log)
+    logger.addHandler(debug_log)
+
+
+def get_line_count(filePath):
+    with open(filePath, 'r') as fp:
+        lines = len(fp.readlines())
+        return lines
+
+
+def validate_file():
+    global id_file
+    with open(id_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.isdigit():
+                logger.error("the id file was not formatted correctly, please make sure it only contains ids with no"
+                             " hyphens")
+
+
+def get_user_language():
+    root = tk.Tk()
+    root.eval('tk::PlaceWindow . center')
+    root.title("Remove Non-Vip Friends")
+    root.geometry('400x200')
+    text = tk.Label(root, text="Select a Language", pady=10)
+    text.pack()
+
+    language_select = ttk.Combobox(root, state="readonly", values=supported_languages)
+    language_select.pack()
+
+    debug_checkbox = ttk.Checkbutton(text="Debug Mode")
+    debug_checkbox.pack(pady=10)
+
+    frame = tk.Frame(root)
+    frame.pack(padx=10, pady=10, expand=True)
+
+    def get_selected_option():
+        global selected_language, is_debug_mode
+        selected_language = language_select.get()
+        is_debug_mode = debug_checkbox.getboolean()
+        root.destroy()
+
+
+
+    cancel_button = ttk.Button(frame, text="Cancel", command=sys.exit)
+    submit_button = ttk.Button(frame, text="Ok", command=get_selected_option)
+    cancel_button.pack(side=tk.LEFT, padx=10)
+    submit_button.pack(side=tk.RIGHT, padx=10)
+    sv_ttk.set_theme("dark")
+    root.mainloop()
